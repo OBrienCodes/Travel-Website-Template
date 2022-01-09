a80ba257ecc4fd63e1bb20ae6fa8402a2a54dd69
Matthew O'Brien <matthew.kevin.obrien@gmail.com>
2022-01-09 01:57:14 -0500

    DaSilveira Diamonds

--- designsbyobrien/src/components/HeroSection.js
+++ designsbyobrien/src/components/HeroSection.js
@@ -7,8 +7,8 @@ import './HeroSection.css'
 function HeroSection() {
     return (
         <div className='hero-container'>
-            <h1>ADVENTURE AWAITS</h1>
-            <p>What are you waiting for?</p>
+            <h1>DaSilveira Diamonds</h1>
+            <p>A Diamond Shines Forever</p>
             <div className='hero-btns'>
                 <Button className='btns' buttonStyle='btn--outline'
                 buttonSize='btn--large'>Get Started</Button>
