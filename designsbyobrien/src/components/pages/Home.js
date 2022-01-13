2f665edb31fd0bc689cc1ac40afc1b3af209a899
Matthew O'Brien <matthew.kevin.obrien@gmail.com>
2022-01-13 14:39:41 -0500

    setting up for deployment

--- designsbyobrien/src/components/pages/Home.js
+++ designsbyobrien/src/components/pages/Home.js
@@ -1,6 +1,7 @@
 import react from 'react';
 import '../../App.css'
 import Cards from '../Cards';
+import Footer from '../Footer';
 import HeroSection from '../HeroSection'
 
 function Home () {
@@ -8,7 +9,7 @@ function Home () {
         <>
         <HeroSection />
         <Cards/>
-        
+        <Footer/>
         
         </>
     );
